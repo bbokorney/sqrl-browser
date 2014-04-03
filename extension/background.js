f88004ef077b8196ef8fca048e0730aac84da25d
Baker Bokorney <bbokorney@gmail.com>
2014-04-03 14:28:29 -0400

    Part of key file uploading works.

--- extension/background.js
+++ extension/background.js
@@ -1,22 +1,38 @@
-function genericOnClick(info, tab) {
-  console.log("item " + info.menuItemId + " was clicked");
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
-
-  qrcode.decode(info.srcUrl);
+function qrCodeRightClick(info, tab) {
+    qrcode.decode(info.srcUrl);
 }
 
 var context = "image";
 var title = "SQRL";
 chrome.contextMenus.create({"title": title,
 							"contexts":[context],
-							"onclick": genericOnClick});
+							"onclick": qrCodeRightClick});
 
 
 qrcode.callback = function(data) {
 	console.log(JSON.stringify(data));
-	
+	url = data.url;
+	// sign this
+	// send a request
 };
 
-chrome.storage.local["test"] = "test value.";
+function setKeyName(name) {
+	chrome.storage.local["keyname"] = name;
+}
+
+function getKeyName() {
+	return chrome.storage.local["keyname"];
+}
+
+chrome.runtime.onMessage.addListener(
+	function(request, send, response) {
+		if(request.action == "newkey") {
+			// store a new key
+		}
+		else if(request.action == "keyname") {
+			response({"keyname": getKeyName()});
+		}
+	}
+
+);
 
--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,32 @@
-var button = document.getElementById("testbtn");
-button.onclick = function() {
-	console.log(chrome.storage.local["test"]);
+function sendMessage(data, callback) {
+	chrome.runtime.sendMessage(data, callback);
 }
+
+function setKeyName(keyname) {
+	document.getElementById("keyName").textContent = keyname == null ? "You don't have a key saved." :
+													"Your current RSA key is " + keyname;
+}
+
+function requestKeyName() {
+	sendMessage({"action": "keyname"}, function(response) {
+		setKeyName(response.keyname);
+	});
+}
+
+function sendNewKey(key) {
+	var data = {"action": "newkey", "key": key};
+	sendMessage(data, function(response){
+		setKeyName(response.keyname);
+	});
+}
+
+function fileChangedHandler(event) {
+	var file = event.target.files;
+	console.log(event.target);
+	console.log(file[0].name);
+}
+
+window.onload = function() {
+	requestKeyName();
+	document.getElementById("keyFileInput").onchange = fileChangedHandler;;
+};
